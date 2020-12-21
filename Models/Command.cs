f0caeea0f189f9fa5b02da9c90e8c4626bd56816
osund <osundal83@gmail.com>
2020-12-21 10:37:20 +0100

    add project Commander

---
+++ Models/Command.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Commander.Models
+{
+    public class Command
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        [MaxLength(250)]
+        public string HowTo { get; set; }
+
+        [Required]
+        public string Line { get; set; }
+        
+        [Required]
+        public string Platform { get; set; }
+    }
+}
